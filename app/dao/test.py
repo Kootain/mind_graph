2f106d0a33ca17539f9ed223e12e16f7abce16d0
kootain.gao <kootain.gao@bytedance.com>
2023-12-15 15:26:41 +0800

    Auto-commit: make commit-push

--- app/dao/test.py
+++ app/dao/test.py
@@ -9,9 +9,39 @@ port = '3734'
 client = client.Client(f'ws://{host}:{port}/gremlin', 'g', username=username, password=password)
 
 
-if __name__ == '__main__':
-
-    callback = client.submit("g.V().limit(1)").all().result()
+def call(q):
+    callback = client.submit(q).all().result()
     for result in callback:
         print(result)
-    client.close()
+
+
+if __name__ == '__main__':
+
+    # 建立点, 点类型 gdb_sample_person, 属性 {id, name}
+    create_points = [
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_alice').property('name', 'Alice').next()"
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_bob').property('name', 'Bob').next()"
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_carol').property('name', 'Carol').next()"
+    ]
+    for g in create_points:
+        call(g)
+
+    # 建立边, 边类型 gdb_sample_knows, 属性 {weight}
+    create_edge = [
+        "g.addE('gdb_sample_knows').from(V('gdb_sample_alice')).to(V('gdb_sample_bob')).property('weight', 0.5f).next()",
+        "g.addE('gdb_sample_knows').from(V('gdb_sample_bob')).to(V('gdb_sample_carol')).property('weight', 0.5f).next()"
+    ]
+    for g in create_edge:
+        call(g)
+
+    onehop_query = "g.V('gdb_sample_alice').out('gdb_sample_knows').toList()"
+    call(onehop_query)
+
+    update_edge_property = "g.E().hasLabel('gdb_sample_knows').where(outV().has(id, 'gdb_sample_alice')).where(inV().has(id, 'gdb_sample_bob')).property('weight', 0.6f).next()"
+    call(update_edge_property)
+
+    drop_point = "g.V('gdb_sample_carol').drop().iterate()"
+    call(drop_point)
+
+    drop_edge = "g.V('gdb_sample_alice').outE('gdb_sample_knows').where(inV().has(id, 'gdb_sample_bob')).drop().iterate()"
+    call(drop_edge)
