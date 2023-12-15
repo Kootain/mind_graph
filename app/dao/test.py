293bdfd560e81e1d1dab3a625da8d08c0d794c4f
kootain.gao <kootain.gao@bytedance.com>
2023-12-15 15:30:12 +0800

    Auto-commit: make commit-push

--- app/dao/test.py
+++ app/dao/test.py
@@ -19,29 +19,29 @@ if __name__ == '__main__':
 
     # 建立点, 点类型 gdb_sample_person, 属性 {id, name}
     create_points = [
-        "g.addV('gdb_sample_person').property(id, 'gdb_sample_alice').property('name', 'Alice')"
-        "g.addV('gdb_sample_person').property(id, 'gdb_sample_bob').property('name', 'Bob')"
-        "g.addV('gdb_sample_person').property(id, 'gdb_sample_carol').property('name', 'Carol')"
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_alice').property('name', 'Alice');"
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_bob').property('name', 'Bob');"
+        "g.addV('gdb_sample_person').property(id, 'gdb_sample_carol').property('name', 'Carol');"
     ]
     for g in create_points:
         call(g)
 
     # 建立边, 边类型 gdb_sample_knows, 属性 {weight}
     create_edge = [
-        "g.addE('gdb_sample_knows').from(V('gdb_sample_alice')).to(V('gdb_sample_bob')).property('weight', 0.5f)",
-        "g.addE('gdb_sample_knows').from(V('gdb_sample_bob')).to(V('gdb_sample_carol')).property('weight', 0.5f)"
+        "g.addE('gdb_sample_knows').from(V('gdb_sample_alice')).to(V('gdb_sample_bob')).property('weight', 0.5f);",
+        "g.addE('gdb_sample_knows').from(V('gdb_sample_bob')).to(V('gdb_sample_carol')).property('weight', 0.5f);"
     ]
     for g in create_edge:
         call(g)
 
-    onehop_query = "g.V('gdb_sample_alice').out('gdb_sample_knows').toList()"
+    onehop_query = "g.V('gdb_sample_alice').out('gdb_sample_knows').toList();"
     call(onehop_query)
 
-    update_edge_property = "g.E().hasLabel('gdb_sample_knows').where(outV().has(id, 'gdb_sample_alice')).where(inV().has(id, 'gdb_sample_bob')).property('weight', 0.6f).next()"
+    update_edge_property = "g.E().hasLabel('gdb_sample_knows').where(outV().has(id, 'gdb_sample_alice')).where(inV().has(id, 'gdb_sample_bob')).property('weight', 0.6f);"
     call(update_edge_property)
 
-    drop_point = "g.V('gdb_sample_carol').drop().iterate()"
+    drop_point = "g.V('gdb_sample_carol').drop().iterate();"
     call(drop_point)
 
-    drop_edge = "g.V('gdb_sample_alice').outE('gdb_sample_knows').where(inV().has(id, 'gdb_sample_bob')).drop().iterate()"
+    drop_edge = "g.V('gdb_sample_alice').outE('gdb_sample_knows').where(inV().has(id, 'gdb_sample_bob')).drop().iterate();"
     call(drop_edge)
