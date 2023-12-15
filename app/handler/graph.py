848e20cb6c690c2d38b1f8b3f4f37437f488c4f5
tangbangjie.benjamin <tangbangjie.benjamin@bytedance.com>
2023-12-16 06:03:49 +0800

    Auto-commit: make commit-push

--- app/handler/graph.py
+++ app/handler/graph.py
@@ -3,15 +3,41 @@ import uuid
 from flask import jsonify, request
 from ..api import api
 from ..service.graph_service import GraphService
+from dataclasses import dataclass
+from gremlin_python.structure.graph import Edge, Vertex
 
 graph_service = GraphService()
+
+
+def clamp(value, min_value, max_value):
+    return max(min_value, min(value, max_value))
+
+
+@dataclass
+class Node:
+    color: str
+    id: str
+    label: str
+    shape: str
+
+
+@dataclass
+class Edge:
+    arrows: str
+    from_node: str  # 'from' is a reserved keyword in Python, so we use 'from_node' instead
+    to: str
+    value: float
 
 
 @api.get('/graph')
 def graph():
     keyword = request.args.get('keyword')
+    depth = request.args.get('depth', 3)
+    depth = clamp(depth, 1, 5)
+
     resp = {
         "input": keyword,
+        "depth": depth,
         'suc': True,
         "reason": ""
     }
@@ -20,7 +46,43 @@ def graph():
         resp["reason"] = "keyword is empty"
         return jsonify(resp)
 
+    ret = graph_service.get_sub_graph(keyword, depth)
+    if not ret:
+        resp['suc'] = False
+        resp["reason"] = "keyword does not exist"
+        return resp
+
+    # node {"color": "#97c2fc", "id": "Python", "label": "Python", "shape": "dot"},
+    # edge {"arrows": "to", "from": "Python", "to": "Java", "value": 0.5},
+
     nodes = {}
     edges = {}
+
+    for r in ret:
+        for obj in r.objects:
+            if isinstance(obj, Edge):
+                from_node = obj.outV
+                to_node = obj.inV
+
+                if from_node.id == to_node.id:
+                    continue
+
+                key = f"{from_node.id}#{to_node.id}"
+                if key not in edges:
+                    edges[key] = {
+                        "arrows": "to",
+                        "from": from_node.id,
+                        "to": to_node.id,
+                        "value": 0.5
+                    }
+
+            if isinstance(obj, Vertex):
+                if obj.id not in nodes:
+                    nodes[obj.id] = {
+                        "color": "#97c2fc",
+                        "id": obj.id,
+                        "label": obj.id,
+                        "shape": "dot",
+                    }
 
     return jsonify({'nodes': list(nodes.values()), 'edge': list(edges.values())})
